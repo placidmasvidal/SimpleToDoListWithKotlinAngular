282a428d4b619823df6aed360cf5a5c4734024da
Placid Masvidal <contact@placidmasvidal.com>
2021-12-28 07:22:05 -0500

    CRUD ops for Todo-list service coded

--- todo/angular-todo/src/app/todo-list/todo-list.component.ts
+++ todo/angular-todo/src/app/todo-list/todo-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { Observable } from 'rxjs';
+import { Todo } from '../models/Todo';
+import { TodoListService } from './todo-list.service';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
+  todos$!: Observable<Todo[]>;
 
-  constructor() { }
+  constructor(
+    private todoListService: TodoListService,
+    private nzMessageService: NzMessageService
+  ) { }
+
+  loadAll = () => {
+    this.todos$ = this.todoListService.findAll();
+  }
+
+  changeStatus(todo: Todo) {
+    this.todoListService.update(todo)
+      .subscribe(() => {
+        this.todos$ = this.todoListService.findAll();
+      });
+    this.nzMessageService.info('Changed Status');
+  }
+
+  deleteTodo(todo: Todo) {
+    this.todoListService.delete(todo.id)
+      .subscribe(() => {
+        this.todos$ = this.todoListService.findAll();
+      });
+    this.nzMessageService.warning('Todo Deleted');
+  }
+
+  cancel(): void {
+    this.nzMessageService.info('Click cancelled');
+  }
 
   ngOnInit(): void {
+    this.todos$ = this.todoListService.findAll();
   }
 
 }
