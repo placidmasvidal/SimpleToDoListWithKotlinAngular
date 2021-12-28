da17242024175f559aee7cab2dbc17a949dca9a2
Placid Masvidal <contact@placidmasvidal.com>
2021-12-28 06:45:58 -0500

    Front-end CRUD operations for Todo coded

--- todo/angular-todo/src/app/todo-list/todo-list.service.ts
+++ todo/angular-todo/src/app/todo-list/todo-list.service.ts
@@ -11,7 +11,25 @@ export class TodoListService {
   constructor(private http: HttpClient) { }
 
   create(todo: Todo): Observable<Todo> {
+    const copy = this.convert(todo);
+    return this.http.post<Todo>(this.resourceUrl, copy);
+  }
 
+  update(todo: Todo): Observable<Todo> {
+    const copy = this.convert(todo);
+    return this.http.put<Todo>(`${this.resourceUrl}/${copy.id}`, copy);
+  }
+
+  find(id: number): Observable<Todo> {
+    return this.http.get<Todo>(`${this.resourceUrl}/${id}`);
+  }
+
+  findAll(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.resourceUrl);
+  }
+
+  delete(id: number | undefined): Observable<HttpResponse<any>> {
+    return this.http.delete<any>(`${this.resourceUrl}/${id}`);
   }
 
   private convert(todo: Todo): Todo {
