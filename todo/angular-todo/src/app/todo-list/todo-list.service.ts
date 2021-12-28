9057139bb88bc0198762c17477685b55d58b157f
Placid Masvidal <contact@placidmasvidal.com>
2021-12-28 06:38:05 -0500

    Post method for create Todo in TodoService coded

--- todo/angular-todo/src/app/todo-list/todo-list.service.ts
+++ todo/angular-todo/src/app/todo-list/todo-list.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Todo } from '../models/Todo';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class TodoListService {
 
-  constructor() { }
+  private resourceUrl = 'http://localhost:8000/todos';
+
+  constructor(private http: HttpClient) { }
+
+  create(todo: Todo): Observable<Todo> {
+
+  }
+
+  private convert(todo: Todo): Todo {
+    const copy: Todo = Object.assign({}, todo);
+    return copy;
+  }
 }
